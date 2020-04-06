9d0f9920d3f54d053414ae6e63c812881eb2c7e8
Abe Pralle <abe.pralle@gmail.com>
2020-04-06 10:21:43 -0700

    [TopicGen] Topic graphics now 1920x1080; renamed temp file
    With generated graphics now the size of my video encoding, I no longer need to adjust their position when adding them in my video editor.

--- TopicGen/TopicGen.js
+++ TopicGen/TopicGen.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   page.setViewport( {width:350,height:1080} );
-  await page.goto('file:///Users/abe/Projects/Tools/TopicGen/Topics.html');
+  await page.goto('file:///Users/abe/Projects/Tools/TopicGen/Temp.html');
   await page.screenshot({path: 'GeneratedImages/Rendered.png'});
 
   await browser.close();
