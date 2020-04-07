d0f3d62ad2903256efea206aec26d9266888ad9a
Abe Pralle <abe.pralle@gmail.com>
2020-04-07 10:45:56 -0700

    [TopicGen] Now outputs 'TitleTopics' for splash image

--- TopicGen/TopicGen.js
+++ TopicGen/TopicGen.js
@@ -1,9 +1,11 @@
 const puppeteer = require('puppeteer');
+let w = Number(process.argv[2]) | 0;
+if (w == 0) w = 350;
 
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  page.setViewport( {width:350,height:1080} );
+  page.setViewport( {width:w,height:1080} );
   await page.goto('file:///Users/abe/Projects/Tools/TopicGen/Temp.html');
   await page.screenshot({path: 'GeneratedImages/Rendered.png'});
 
